96c6753ad96c6f8ba19d4e1e1802724b9c50a08d
Ted Lorts <tlorts@cranbrook.edu>
2016-12-17 21:28:29 -0500

    taking away a few debug prints

--- common/models/organization.js
+++ common/models/organization.js
@@ -5,20 +5,20 @@ module.exports = function(Organization) {
 
 	Organization.observe('before delete', function(ctx, next){
 		var Division = app.models.Division;
-		console.log('Organization before delete, where:', ctx.where);
+		// console.log('Organization before delete, where:', ctx.where);
 		
 		Division.find({where: {organizationId: ctx.where.id}})
 		
 		.catch(next)
 		.then(function(divisions){
-			console.log('destroying divisions', divisions);
+			// console.log('destroying divisions', divisions);
 			var tasks = divisions.map(d => d.destroy());
 			return Promise.all(tasks);
 		})
 
 		.catch(next)
 		.then(function(result){
-			console.log('done destroying divisions', result);
+			// console.log('done destroying divisions', result);
 			next();
 		});
 	});
