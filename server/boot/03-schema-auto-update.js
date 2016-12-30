757da7679573a21d1e273bc294b7456143806468
Ted Lorts <tlorts@cranbrook.edu>
2016-12-30 09:52:37 -0500

    tabs to spaces and option to migrate instead of update

--- server/boot/03-schema-auto-update.js
+++ server/boot/03-schema-auto-update.js
@@ -1,25 +1,56 @@
 'use strict';
 
-module.exports = function(app) {
-	var pg = app.dataSources.pg;
-	console.info('Checking database schema');
-	pg.isActual(null, function(err, actual){
-		if(err){
-			console.log('problem running isActual');
-			console.log(err);
-		} else {
-			if(!actual){
-				pg.autoupdate(null, function(err, result){
-					if(err){
-						console.log('problem running autoupdate');
-						console.log(err);
-					} else{
-						console.log(result);
-					}
-				});
-			} else {
-				console.log('It is "actual"');
-			}
-		}
-	});
+module.exports = function(app, next) {
+  console.log('________________________________');
+  console.info('Checking database schema');
+  var pg = app.dataSources.pg;
+  const resetDatabase = false;
+
+  if (resetDatabase) {
+    pg.automigrate()
+    .catch(err => {
+      console.log('problem during automigrate:', err);
+      next(err);
+    })
+    .then(() => {
+      console.log('migration complete');
+      next();
+    });
+    return;
+  }
+
+  new Promise((resolve, reject) => {
+    console.log('running isActual');
+    pg.isActual((err, actual) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(actual);
+      }
+    });
+  })
+
+  .catch(err => {
+    console.log('problem running isActual: ', err);
+    next(err);
+  })
+  .then(schemaIsActual => {
+    if (schemaIsActual) {
+      console.log('Database schema is already matching the models. [%s]', schemaIsActual);
+      return null;
+    }
+
+    console.log('Running autoupdate');
+    return pg.autoupdate();
+  })
+
+  .catch(err => {
+    console.log('problem running autoupdate: ', err);
+    next(err);
+  })
+  .then(result => {
+    console.log('Done checking/updating database schema: ', result);
+    next();
+  });
+
 };
