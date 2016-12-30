d289645bb435ae2b3775fc7884cd682e5e28c119
Ted Lorts <tlorts@cranbrook.edu>
2016-12-30 09:53:14 -0500

    generalized recursive function to create heirachical structure in db from json

--- server/boot/04-populate-database.js
+++ server/boot/04-populate-database.js
@@ -4,63 +4,71 @@ var orgTemplate = require('../../local-data/roster.json');
 var _ = require('lodash');
 
 
-module.exports = function(app) {
-	var Organization = app.models.Organization;
+function deepCreate(Model, data, parentIdForeignKey, parentId) {
+  let x = 5;
+  const y = 2;
+  return Promise.all(data.map(object => {
+    var relationKeys = [];//can be concise with a map call and object.getOwnPropertyNames
+    for (var key in object){
+      var relationInfo = Model.relations[key];
+      if (relationInfo) {
+        relationKeys.push(key);
+        //TODO: check that object[key] is an array
+      }
+    }
+    var thisObjectOnly = _.omit(object, relationKeys);
+    if (parentIdForeignKey) {
+      thisObjectOnly[parentIdForeignKey] = parentId;
+    }
 
-	var orgWithName = _.pick(orgTemplate, 'name');
+    return Model.create(thisObjectOnly)
+    .catch(err => {
+      console.log('Problem in creating %s\nError: %s', thisObjectOnly, err);
+    })
+    .then(dbObject => {
+      var childPromises = [];
+      relationKeys.forEach(rKey => {
+        var relationInfo = Model.relations[rKey];
+        var ChildModel = relationInfo.modelTo;
+        var childrenData = object[rKey];
+        var fkOfChild = relationInfo.keyTo;
+        var id = dbObject[relationInfo.keyFrom];
+        childPromises.push(deepCreate(ChildModel, childrenData, fkOfChild, id));
+      });
+      return Promise.all(childPromises);
+    });
+  }));
+}
 
-	Organization.findOrCreate( {
-		where: orgWithName,
-		include: {divisions: {teams: {
-			members: {}//['surname', 'givenName', 'preferredName']
-		}}}
-	}, orgWithName )
-	
-	.catch(function(err){
-		console.log('problem getting orgs', err);
-	})
-	.then(function(params){
-		var org = params[0];
-		var orgData = org.toJSON();
-		// console.log(JSON.stringify(orgData, null, 2));
-		var divisionsToPut = _.differenceBy(orgTemplate.divisions, orgData.divisions, 'name');
-		divisionsToPut = divisionsToPut.map( (d) => _.pick(d, 'name') );
-		console.log('Create these divisions', divisionsToPut);
-		return org.divisions.create(divisionsToPut)
-	})
 
-	.catch(function(err){
-		console.log('problem creating divisions', err);
-	})
-	.then(function(createdDivisions) {
-		//console.log(createdDivisions);
-		var teamCreationPromises = [];
-		createdDivisions.forEach(function(d){
-			let divisionTeams = _.find(orgTemplate.divisions, _.pick(d, 'name')).teams;
-			let teamsToMake = divisionTeams.map( (t) => _.pick(t, ['numericName', 'name']) );
-			console.log('Team to make for '+d.name, teamsToMake);
-			let divisionTeamsPromise = d.teams.create(teamsToMake)
-			.catch(function(err){
-				console.log('problem creating one set of teams', err);
-			})
-			.then(function(createdTeams){
-				let memberPromises = [];
-				createdTeams.forEach(function(team){
-					let membersToMake = _.find(divisionTeams, _.pick(team, 'numericName')).members;
-					memberPromises.push( team.members.create(membersToMake) );
-				});
-				return Promise.all(memberPromises);
-			});
-			teamCreationPromises.push(divisionTeamsPromise);
-		});
-		return Promise.all(teamCreationPromises);
-	})
+module.exports = function(app, next) {
+  console.log('________________________________');
+  console.info('Checking for starter data.');
 
-	.catch(function(err){
-		console.log('problem creating teams', err);
-	})
-	.then(function(teamSets){
-		console.log('done making teamses');
-		console.log(teamSets);
-	});
+  var Organization = app.models.Organization;
+
+  Organization.findOne({where: {name: orgTemplate.name}})
+  .catch(err => {
+    console.log('problem when finding Organization:', err);
+    next(err);
+  })
+  .then(foundOrg => {
+    console.log('got this:', foundOrg);
+    var toCreate = [];
+    if (foundOrg === null) {
+      toCreate.push(orgTemplate);
+    }
+    console.log('Going to create:', toCreate);
+    return deepCreate(Organization, toCreate);
+  })
+
+  .catch(err => {
+    console.log('Problem in creating initial data:', err);
+    next(err);
+  })
+  .then(result => {
+    console.log('Done creating initial data.');
+    next();
+  });
+
 };
